4ae0b9c74d5f9e00b23d578153c31907f67ef20b
arash-malekzadeh <arash.malekzade@gmail.com>
2024-07-27 20:20:24 +0330

    admin register

--- backend/quiz/settings.py
+++ backend/quiz/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'autoslug',
+    'quizApp',
 ]
 
 MIDDLEWARE = [
--- backend/quizApp/admin.py
+++ backend/quizApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question,Quiz,Answer
+
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display=["id","title"]
+
+admin.site.register(Quiz,QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
 
 # Register your models here.
